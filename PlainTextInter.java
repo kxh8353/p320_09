9d2057ef602b498c48f8c55ab762389a664e0486
kxh8353 <kxh8353@rit.edu>
2024-10-17 09:02:47 -0400

    added collection file

--- PlainTextInter.java
+++ PlainTextInter.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 import java.util.Scanner;
-
+ 
 public class PlainTextInter {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
