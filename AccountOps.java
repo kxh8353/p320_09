049116ef2548149d20d48e00e02021125e9b266e
kxh8353 <kxh8353@rit.edu>
2024-10-29 07:59:10 -0400

    push to main

--- AccountOps.java
+++ AccountOps.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class AccountOps {
 
-    private static final String DatabaseURL = "jdbc:mysql://localhost:127.0.0.1/p320_09";
+    private static final String DatabaseURL = "jdbc:postgresql://127.0.0.1:5432/p320_09";
     private static final String DatabaseUSERNAME = System.getenv("DB_USERNAME");
     private static final String DatabasePASSWORD = System.getenv("DB_PASSWORD");
 
@@ -34,21 +34,28 @@ public class AccountOps {
         System.out.print("Enter new password: ");
         String password = scanner.nextLine();
 
+        
+        try{
 
-        try (Connection conn = DriverManager.getConnection(DatabaseURL, DatabaseUSERNAME, DatabasePASSWORD)) {
-            String query = "INSERT INTO users (username, password) VALUES (USERNAME, PASSWORD)";
-            PreparedStatement statement = conn.prepareStatement(query);
-            statement.setString(1, username);
-            statement.setString(2, password);
-            int rowsAffected = statement.executeUpdate();
+            Class.forName("org.postgresql.Driver");
 
-            if (rowsAffected > 0) {
-                System.out.println("Account created successfully!");
-            } else {
-                System.out.println("Account creation failed. Username might already exist.");
+            try (Connection conn = DriverManager.getConnection(DatabaseURL, DatabaseUSERNAME, DatabasePASSWORD)) {
+                String query = "INSERT INTO users (username, password) VALUES (USERNAME, PASSWORD)";
+                PreparedStatement statement = conn.prepareStatement(query);
+                statement.setString(1, username);
+                statement.setString(2, password);
+                int rowsAffected = statement.executeUpdate();
+
+                if (rowsAffected > 0) {
+                    System.out.println("Account created successfully!");
+                } else {
+                    System.out.println("Account creation failed. Username might already exist.");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (Exception f){
+            f.printStackTrace();
         }
     }
 
