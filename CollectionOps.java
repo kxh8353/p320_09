f8215631df8832179d94248d528353a5f8ae3134
Kaitlynn Clement <kmc1191@rit.edu>
2024-10-31 08:53:51 -0400

    added rest of methods to collectionops

--- CollectionOps.java
+++ CollectionOps.java
@@ -7,10 +7,23 @@ public class CollectionOps {
     public static void SeeCollection(){
 
     }
+    
     public static void ChangeCollection(){
 
+    }
+
+    public static void AddMovieCollection(){
 
     }
+
+    public static void DeleteMovieCollection(){
+
+    }
+
+    public static void ModifyCollectionName(){
+
+    }
+
 }
 
 
