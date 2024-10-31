42e1eec01594f4ca8ea8f1c1cf66f24dad383dab
kxh8353 <kxh8353@rit.edu>
2024-10-31 11:52:55 -0400

    moved login finctiuon into accountops

--- AccountOps.java
+++ AccountOps.java
@@ -38,8 +38,47 @@ public class AccountOps {
         }
     }*/
     static boolean login(Connection conn){
-        return false;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("welcome to MovieMatrix, let's help you log in");
+
+        while (true){
+            System.out.println("\nEnter command in the format: Login <username> <password>");
+            String command = scanner.nextLine();
+            String[] userIn = command.split(" ");
+
+            if (userIn[0].equalsIgnoreCase("Login")){
+                String username = userIn[1];
+                String password = userIn[2];
+
+                login(conn, username, password);
+            }else{
+                System.out.println("Unknown command or incorrect usage. Please use 'Login <username> <password>'");
+            }
+        }
     }
+
+    static void login(Connection conn, String username, String password){
+        String query = "SELECT password FROM users WHERE username = ?";
+
+        try(PreparedStatement stmt = conn.prepareStatement(query)){
+            stmt.setString(1, username);
+            ResultSet resultset = stmt.executeQuery();
+
+            if (resultset.next()){
+                String storedPassword = resultset.getString("password");
+                if (storedPassword.equals(password)){
+                    System.out.println("Login successful for user: " + username);
+                }else{
+                    System.out.println("Login failed. incorrect password");
+                }
+            }else{
+                System.out.println("Login failed: username does not exist");
+            }
+        } catch (SQLException e){
+            System.out.println(e);
+        }
+    }
+    
     static void createAccount(Connection conn, String username, String password, String firstname, String lastname) {
 
 //        try {
