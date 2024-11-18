1faba7266605fd833487cf4c37c6cfa277380ced
kxh8353 <kxh8353@rit.edu>
2024-11-18 03:43:46 -0500

    fixed typos in movierecommendations

--- MovieReccomendation.java
+++ MovieReccomendation.java
@@ -74,81 +74,82 @@ public class MovieReccomendation {
     // (e.g. genre, cast member, rating) and the play history of similar users
     public static void RecommendedMoviesForYou(Connection conn, int uid) {
         String query = "SELECT " +
-        "   m.movieid, " +
-        "   m.title, " +
-        "   AVG(r.number_of_stars) AS avg_rating" +
-        "FROM movies m " +
-        "LEFT JOIN has_genre hg ON m.movieid = hg.movieid " +
-        "LEFT JOIN genre g ON hg.genreid = g.genreid " +
-        "LEFT JOIN rates r ON m.movieid = r.movieid " +
-        "WHERE hg.genreid IN ( " +
-            "SELECT g.genreid " +
-            "FROM watched w " +
-            "JOIN has_genre hg ON w.movieid = hg.movieid " +
-            "JOIN genre g ON hg.genreid = g.genreid " +
-            "WHERE w.uid = ? " +
-            "GROUP BY g.genreid " +
-        ") " +
-        "GROUP BY m.movieid, m.title " +
-        "UNION " +
-        "SELECT " +
-        "   m.movieid " +
-        "   m.title " +
-        "   AVG(r.number_of_stars) AS avg_rating" +
-        "FROM movies m " +
-        "LEFT JOIN casts AS cs ON m.movieid = cs.movieid " +
-        "LEFT JOIN rates AS r ON m.movieid = r.movieid " +
-        "WHERE cs.contributorid IN ( "  +
-            "SELECT c.contributorid " +
-            "FROM watched w " +
-            "JOIN casts cs ON w.movieid = cs.movieid " + 
-            "JOIN contributors c ON cs.contributorid = c.contributorid " + 
-            "JOIN has_genre hg ON w.movieid = hg.movieid " +
-            "GROUP BY c.contributorid " +
-        ") " +
-        "GROUP BY m.movieid, m.title " +
-        "UNION " +
-        "SELECT "  +
-        "   m.movieid, " +
-        "   m.title, " +
-        "   AVG(r.number_of_stars) AS avg_rating "  +
-        "FROM movies m " +
-        "LEFT JOIN rates AS r ON m.movieid = r.movieid " +
-        "WHERE m.movieid IN ( " +
-            "SELECT m.movieid " + 
-            "FROM watched w " + 
-            "JOIN movies m ON w.movieid = m.movieid " + 
-            "LEFT JOIN rates r ON m.movieid = r.movieid " + 
-            "WHERE w.uid = ? " + 
-            "GROUP BY m.movieid, m.title " + 
-            "ORDER BY AVG(r.number_of_stars) DESC " + 
-            "LIMIT 10 " +
-        ") " +
-        "GROUP BY m.movieid, m.title " + 
-        "UNION " + 
-        "SELECT " +
-        "   m.movieid, " +
-        "   m.title, " +
-        "   AVG(r.number_of_stars) AS avg_rating " + 
-        "FROM movies m " + 
-        "JOIN watched w ON m.movieid = w.movieid" + 
-        "LEFT JOIN rates r ON m.movieid = r.movieid" + 
-        "WHERE w.uid in ( " + 
-            "SELECT w2.uid " + 
-            "FROM watched w " + 
-            "JOIN watched w2 ON w.movieid = w2.movieid " + 
-            "WHERE w.uid = ? " + 
-            "GROUP BY w2.uid " + 
-        ") " + 
-        "GROUP BY m.movieid, m.title " +
-        "HAVING AVG(r.number_of_stars) IS NOT NULL " +
-        "ORDER BY avg_rating DESC " + 
-        "LIMIT 10 ";
+                "   m.movieid, " +
+                "   m.title, " +
+                "   AVG(r.number_of_stars) AS avg_rating " +
+                "FROM movies m " +
+                "LEFT JOIN has_genre hg ON m.movieid = hg.movieid " +
+                "LEFT JOIN genre g ON hg.genreid = g.genreid " +
+                "LEFT JOIN rates r ON m.movieid = r.movieid " +
+                "WHERE hg.genreid IN ( " +
+                "    SELECT g.genreid " +
+                "    FROM watched w " +
+                "    JOIN has_genre hg ON w.movieid = hg.movieid " +
+                "    JOIN genre g ON hg.genreid = g.genreid " +
+                "    WHERE w.uid = ? " +
+                "    GROUP BY g.genreid " +
+                ") " +
+                "GROUP BY m.movieid, m.title " +
+                "UNION " +
+                "SELECT " +
+                "   m.movieid, " +  
+                "   m.title, " +    
+                "   AVG(r.number_of_stars) AS avg_rating " +
+                "FROM movies m " +
+                "LEFT JOIN casts AS cs ON m.movieid = cs.movieid " +
+                "LEFT JOIN rates AS r ON m.movieid = r.movieid " +
+                "WHERE cs.contributorid IN ( "  +
+                "    SELECT c.contributorid " +
+                "    FROM watched w " +
+                "    JOIN casts cs ON w.movieid = cs.movieid " +
+                "    JOIN contributors c ON cs.contributorid = c.contributorid " +
+                "    JOIN has_genre hg ON w.movieid = hg.movieid " +
+                "    GROUP BY c.contributorid " +
+                ") " +
+                "GROUP BY m.movieid, m.title " +
+                "UNION " +
+                "SELECT "  +
+                "   m.movieid, " +
+                "   m.title, " +
+                "   AVG(r.number_of_stars) AS avg_rating "  +
+                "FROM movies m " +
+                "LEFT JOIN rates AS r ON m.movieid = r.movieid " +
+                "WHERE m.movieid IN ( " +
+                "    SELECT m.movieid " +
+                "    FROM watched w " +
+                "    JOIN movies m ON w.movieid = m.movieid " +
+                "    LEFT JOIN rates r ON m.movieid = r.movieid " +
+                "    WHERE w.uid = ? " +
+                "    GROUP BY m.movieid, m.title " +
+                "    ORDER BY AVG(r.number_of_stars) DESC " +
+                "    LIMIT 10 " +
+                ") " +
+                "GROUP BY m.movieid, m.title " +
+                "UNION " +
+                "SELECT " +
+                "   m.movieid, " +
+                "   m.title, " +
+                "   AVG(r.number_of_stars) AS avg_rating " +
+                "FROM movies m " +
+                "JOIN watched w ON m.movieid = w.movieid " +
+                "LEFT JOIN rates r ON m.movieid = r.movieid " +
+                "WHERE w.uid in ( " +
+                "    SELECT w2.uid " +
+                "    FROM watched w " +
+                "    JOIN watched w2 ON w.movieid = w2.movieid " +
+                "    WHERE w.uid = ? " +
+                "    GROUP BY w2.uid " +
+                ") " +
+                "GROUP BY m.movieid, m.title " +
+                "HAVING AVG(r.number_of_stars) IS NOT NULL " +
+                "ORDER BY avg_rating DESC " +
+                "LIMIT 10";
         
 
         try (PreparedStatement stmt = conn.prepareStatement(query)){
             stmt.setInt(1, uid);
             stmt.setInt(2, uid);
+            stmt.setInt(3, uid); 
 
             ResultSet rs = stmt.executeQuery();
             System.out.println("Top 10 Movies Recommended For You: ");
